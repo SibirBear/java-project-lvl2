eeadf2e54cfb2f3996e78a716faaed94aea12b8a
SibirBear_Linux <c.igor@list.ru>
2022-02-06 01:22:44 +0700

    step 3

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+
+@Command(mixinStandardHelpOptions = true, version = "auto help demo - picocli 3.0",
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Runnable {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.parseArgs(args);
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+        } else if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+        }
+    }
+
+    @Override
+    public void run() {
+
     }
 }
+
