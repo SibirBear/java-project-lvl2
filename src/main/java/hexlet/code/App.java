458cf8159145cb8aa55f66a4fd23ccc621acbb9f
SibirBear_Linux <c.igor@list.ru>
2022-02-07 01:59:55 +0700

    step 4

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,12 +3,23 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import static picocli.CommandLine.*;
 
-@Command(mixinStandardHelpOptions = true, version = "auto help demo - picocli 3.0",
+
+@Command(mixinStandardHelpOptions = true, version = "1.0",
         name = "gendiff",
         description = "Compares two configuration files and shows a difference.")
 
 public class App implements Runnable {
+
+    @Option(names = {"-f", "--format"},description = "output format [default: stylish]")
+    private String format;
+
+    @Parameters(description = "path to first file")
+    private String filepath1;
+
+    @Parameters(description = "path to second file")
+    private String filepath2;
 
     public static void main(String[] args) {
         CommandLine commandLine = new CommandLine(new App());
